d3b74d00e32232b5592528859225fccc1d86781e
Alexander <alexander@Alex.local>
2020-10-13 01:43:00 +0700

    ok

--- App.js
+++ App.js
@@ -6,22 +6,11 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createStackNavigator} from '@react-navigation/stack';
 
-
-
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
-
-
-
-
 export default class App extends Component {
-
-
-
   render() {
-
-
   createHomeStack = () =>
   <Stack.Navigator>
     <Stack.Screen name="Feed" component= {Feed} />
@@ -32,19 +21,9 @@ export default class App extends Component {
         <Drawer.Navigator>
         <Drawer.Screen name="Home" children={createHomeStack}  />
           <Drawer.Screen name="Menu" component={Menu}/>
-
           <Drawer.Screen name="Contact" component={Contact}/>
-
         </Drawer.Navigator>
-
       </NavigationContainer>
-
-
-
-
       )
   }
 }
-
-
-
